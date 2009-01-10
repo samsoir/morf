ae67d953dcd3ba03dccd568b00c95cac8185b26e
Sam Clark <clarks@venus.gateway.2wire.net>
2009-01-10 18:57:20 +0000

    Added license notification to Morf

--- libraries/Morf.php
+++ libraries/Morf.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Morf
+ * Yet another form library
+ *
+ * @package default
+ * @author Sam Clark
+ * @license GNU Public License v3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
 
 class Morf_Core
 {
