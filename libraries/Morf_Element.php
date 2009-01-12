dd0b84a046144e11941c19a9076775b5648e072d
Sam Clark <clarks@venus.local>
2009-01-12 14:37:51 +0000

    Fixed the error handling so that errors now display

--- libraries/Morf_Element.php
+++ libraries/Morf_Element.php
@@ -1,5 +1,14 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-
+/**
+ * Morf_Element
+ * Yet another form library
+ *
+ * This abstract class provides the base framework for almost all other Morf form elements.
+ *
+ * @package default
+ * @author Sam Clark
+ * @license GNU Public License v3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
 abstract class Morf_Element_Core
 {
 	// Configuration
@@ -29,6 +38,17 @@ abstract class Morf_Element_Core
 	// Post value
 	protected $post;
 
+	/**
+	 * Factory method for generating Morf_Element derivatives
+	 *
+	 * @param string $element The name of the element (use one of the predifined element types from config/morf)
+	 * @param array $config The configuration for this element as an associative array
+	 * @param mixed $value The value to assign to the element
+	 * @param string $selected Will be depreciated soon (ignore)
+	 * @param array $post The post array of elements to use the post values rather than the predefined values
+	 * @return Morf_Element object
+	 * @author Sam Clark
+	 */
 	public function factory($element, $config = array(), $value = NULL, $selected = NULL, $post = FALSE)
 	{
 		$model = 'Morf_'.ucfirst($element);
@@ -47,11 +67,32 @@ abstract class Morf_Element_Core
 		return $obj;
 	}
 
+	/**
+	 * The Morf_Element base contructor
+	 *
+	 * @param array $config Configuration array of keys => values
+	 * @param mixed $value The value of this element
+	 * @param boolean $post if TRUE, this element will load it's value from the post element of the same name (if it exists)
+	 * @param array $post If this elements name is a key within the $post array, the value will be taken from that key
+	 * @return void
+	 * @author Sam Clark
+	 */
 	public function __construct($config = array(), $value = NULL, $post = FALSE)
 	{
 		$this->_init($config, $value, $post);
+		return;
 	}
-	
+
+	/**
+	 * Initialises this Morf_Element
+	 *
+	 * @param array $config Configuration array of keys => values
+	 * @param string $value The value of this element
+	 * @param boolean $post if TRUE, this element will load it's value from the post element of the same name (if it exists)
+	 * @param array $post If this elements name is a key within the $post array, the value will be taken from that key
+	 * @return void
+	 * @author Sam Clark
+	 */
 	protected function _init($config, $value, $post)
 	{
 		$this->post = $post;
@@ -62,15 +103,23 @@ abstract class Morf_Element_Core
 			{
 				$this->attributes[$key] = $val;
 			}
-			elseif (property_exists( $this, $key))
+			elseif (property_exists($this, $key))
 			{
 				$this->$key = $val;
 			}
 		}
 		
 		$this->value = $value;
+		return;
 	}
-	
+
+	/**
+	 * Overloads the __get() method to allow access to this elements attributes and value
+	 *
+	 * @param string $key The attribute name or value to retrieve
+	 * @return mixed $value The resulting value
+	 * @author Sam Clark
+	 */
 	public function __get($key)
 	{
 		$value = FALSE;
@@ -87,6 +136,14 @@ abstract class Morf_Element_Core
 		return $value;
 	}
 
+	/**
+	 * Overloads the __set() method to allow setting of classes, attributes or the value
+	 *
+	 * @param string $key The property to set
+	 * @param mixed $value The value
+	 * @return void
+	 * @author Sam Clark
+	 */
 	public function __set($key, $value)
 	{
 		if ($key === 'class')
@@ -101,15 +158,13 @@ abstract class Morf_Element_Core
 		{
 			$this->value = $value;
 		}
-
-		return $this;
 	}
 
 	/**
 	 * Set a class name to the class attribute. This method automatically handles multiple class names
 	 *
-	 * @param string $value 
-	 * @return this
+	 * @param string $value The class name to add to the class attribute
+	 * @return Morf_Element $this
 	 * @author Sam Clark
 	 */
 	public function add_class($value = FALSE)
@@ -251,7 +306,7 @@ abstract class Morf_Element_Core
 				$this->_process_post();
 
 			// Discover if there is an error
-			if (isset($this->name) AND $errors AND array_key_exists($this->name, $errors))
+			if (isset($this->attributes['name']) AND $errors AND array_key_exists($this->name, $errors))
 			{
 				$this->add_class(Kohana::config('morf.errors.class'));
 				$result['template']->error = $errors[$this->name];
